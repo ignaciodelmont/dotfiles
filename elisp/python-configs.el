29c6305bf83fc76bd814dd578618565bdcb3f856
Ignacio Delmont <ignaciodelmont@gmail.com>
2024-04-19 09:19:12 -0300

    comment out dap-debug template causing errors everwhere, add todo

--- elisp/python-configs.el
+++ elisp/python-configs.el
@@ -33,13 +33,14 @@
   :bind (:map python-mode-map
 	      ("s-b" . python-black-buffer)
 	      ("s-r" . python-black-region)))
-  
-(dap-register-debug-template "Kognitos brain"
-  (list :type "python"
-        :args ("-k" "test_ocr_text_is_in")
-	:cwd (expand-file-name "~/Projects/Kognitos/brain/")
-	:env '(("KOGNITOS_ENVIRONMENT" . "shared")("AWS_PROFILE" . "kognitos")("AWS_DEFAULT_REGION" . "us-west-2"))
-	:debugger "debugpy"
-        :request "launch"
-	:module "pytest"
-        :name "Test"))
+
+;; TODO: Move this to the project itself
+;; (dap-register-debug-template "Kognitos brain"
+;;   (list :type "python"
+;;         :args ("-k" "test_ocr_text_is_in")
+;; 	:cwd (expand-file-name "~/Projects/Kognitos/brain/")
+;; 	:env '(("KOGNITOS_ENVIRONMENT" . "shared")("AWS_PROFILE" . "kognitos")("AWS_DEFAULT_REGION" . "us-west-2"))
+;; 	:debugger "debugpy"
+;;         :request "launch"
+;; 	:module "pytest"
+;;         :name "Test"))
