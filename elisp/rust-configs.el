7c5b7b2bd045704f339df53c98b726c0c7c0a677
Ignacio Delmont <ignaciodelmont@gmail.com>
2026-01-04 10:40:31 -0300

    enhance rust configs

--- elisp/rust-configs.el
+++ elisp/rust-configs.el
@@ -1,2 +1,8 @@
-(use-package rustic)
-(setq rustic-lsp-client 'eglot)
+(use-package rustic
+  :config
+  (setq rustic-format-on-save nil)
+  (setq rustic-lsp-client 'eglot)
+  :hook (rustic-mode . (lambda()
+                    (flycheck-select-checker 'rustic-clippy)))
+  :custom
+  (rustic-cargo-use-last-stored-arguments t))
